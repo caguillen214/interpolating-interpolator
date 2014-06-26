373b2e5268481197a1ca5d6091fe4638c1217792
Carlo s A. Guillen <caguillen@google.com>
2014-06-26 11:30:20 -0700

    feat(decorator.js): implement decorator for $interpolate to log undefined variables

--- decorator.js
+++ decorator.js
@@ -1,19 +1,36 @@
-angular.module('iEyeLib', [])
-  .config(['$provide',function($provide) {
+angular.module('iEyeLib', ['testModule', 'ngRoute'])
+  .config(['$provide', '$routeProvider', function($provide, $routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'iEyeController',
+        controllerAs: 'iCtrl',
+        templateUrl: 'iEyeDemo.html'
+      }).
+      otherwise({redirectTo: '/'});
     $provide.decorator('$interpolate', ['$delegate', function($delegate) {
       var interpolateWrapper = function() {
         var interpolationFn = $delegate.apply(this, arguments);
         if(interpolationFn) {
-            return interpolationFnWrap(interpolationFn, arguments);
+          var argC = arguments.callee;
+          var parts = iEye.getAllParts(arguments[0],argC.startSymbol(),argC.endSymbol());
+          var results = [];
+          var temp = interpolationFnWrap(interpolationFn,arguments, parts);
+          return temp;
         }
       }
-      var interpolationFnWrap = function(interpolationFn, interpolationArgs) {
-        return function() {
-          var results = interpolationFn.apply(this,arguments);
-          var vals = interpolationArgs[0].trim();
-          console.log(vals);
-          return results;
-        }
+      var interpolationFnWrap = function(interpolationFn, interpolationArgs, allParts) {
+        return function(){
+          var result = interpolationFn.apply(this, arguments);
+          var original = interpolationArgs[0];
+          var args = arguments[0];
+          var found = false;
+          allParts.forEach(function(part) {
+            if(!args.$eval(part) && !found){
+              found = true;
+              console.warn(part+' was found to be undefined in "'+original.trim()+'".');
+            }
+          })
+          return result;
+        };
       }
       angular.extend(interpolateWrapper,$delegate);
       return interpolateWrapper;
--- ieye-lib/ieye-lib.js
+++ ieye-lib/ieye-lib.js
@@ -6,19 +6,29 @@ iEye = {
   car:car
 }
 
-iEye.checkInterpolation = function(text, scope) {
-  var errors =[];
-  var parts = iEye.getParts(text);
-  parts.forEach(function(operand) {
+iEye.getAllParts = function(text, startSym, endSym) {
+  var comboParts = [];
+  var interpolation = iEye.getInterpolation(text, startSym, endSym);
+  var operands = iEye.getOperands(interpolation);
+  operands.forEach(function(operand) {
     var opParts =  operand.split('.');
-    var result = iEye.checkExsistance(iEye, opParts[0], opParts, 0, "");
-    if(result){
-      iEye.displayResults(result, text);
+    for(var i = 0; i < opParts.length; i++) {
+      var result = iEye.concatParts(opParts,i);
+      if(result && comboParts.indexOf(result) < 0){
+        comboParts.push(result);
+      }
     }
   })
+  return comboParts;
 }
 
-iEye.getParts = function(str) {
+iEye.getInterpolation = function(text, startSym, endSym) {
+  var startInd = text.indexOf(startSym) + startSym.length;
+  var endInd = text.indexOf(endSym);
+  return text.substring(startInd, endInd);
+}
+
+iEye.getOperands = function(str) {
   return str.split(/[^a-zA-Z_.]/).filter(function(x){return x;});
 }
 
@@ -45,5 +55,3 @@ iEye.concatParts = function(parts,concatLength) {
 iEye.displayResults = function(result, interpolation) {
   console.log(result+ ' was found to be undefined in '+interpolation);
 }
-iEye.checkInterpolation('{{car.detail.brand+car.details.brand+car.wheels("4")/2}}')
-// console.log();
