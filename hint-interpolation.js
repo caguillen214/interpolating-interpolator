6f6efd9d9d6cbbce5225e71ea5a788745f9a1b48
Carlo s A. Guillen <caguillen@google.com>
2014-07-17 18:13:49 -0700

    fix(*): reimplement changes

--- hint-interpolation.js
+++ hint-interpolation.js
@@ -1,25 +1,18 @@
 'use strict';
-angular.module('ngHintInterpolation', ['testModule', 'ngRoute'])
-  .config(['$provide', '$routeProvider', function($provide, $routeProvider) {
+
+angular.module('ngHintInterpolation', [])
+  .config(['$provide', function($provide) {
     var ngHintInterpMessages = [];
-    $routeProvider.when('/', {
-        controller: 'iiLibController',
-        controllerAs: 'iCtrl',
-        templateUrl: 'components/iiLibDemo.html'
-      }).
-      otherwise({redirectTo: '/'});
-    $provide.decorator('$interpolate', ['$delegate', '$timeout','$rootScope',
-      function($delegate, $timeout, $compile, $rootScope) {
+    $provide.decorator('$interpolate', ['$delegate', '$timeout',
+      function($delegate, $timeout) {
         var interpolateWrapper = function() {
           var interpolationFn = $delegate.apply(this, arguments);
           if(interpolationFn) {
-            var argC = arguments.callee;
-            var parts = iiLib.getAllParts(arguments[0],argC.startSymbol(),argC.endSymbol());
-            var results = [];
+            var parts = iiLib.getAllParts(arguments[0],$delegate.startSymbol(),$delegate.endSymbol());
             var temp = interpolationFnWrap(interpolationFn,arguments, parts);
             return temp;
           }
-        }
+        };
         var interpolationFnWrap = function(interpolationFn, interpolationArgs, allParts) {
           return function(){
             var result = interpolationFn.apply(this, arguments);
@@ -32,15 +25,15 @@ angular.module('ngHintInterpolation', ['testModule', 'ngRoute'])
                 found = true;
                 message = '"'+part+'" was found to be undefined in "'+original.trim()+'".';
               }
-            })
+            });
             if(message && ngHintInterpMessages.indexOf(message) < 0) {
               ngHintInterpMessages.push(message);
               ngHintInterpMessages.push(args.$parent);
-              iiLib.delayDisplay(ngHintInterpMessages);
+              iiLib.delayDisplay(ngHintInterpMessages, $timeout);
             }
             return result;
           };
-        }
+        };
         angular.extend(interpolateWrapper,$delegate);
         return interpolateWrapper;
     }]);
