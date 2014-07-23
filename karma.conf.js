823ca493ee8ea0d613784ae14596c2357c5d5b5e
Carlo s A. Guillen <caguillen@google.com>
2014-07-23 15:26:16 -0700

    fix(karma.conf): fix unexpected string on line 21

--- karma.conf.js
+++ karma.conf.js
@@ -17,8 +17,8 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'test/*_test.js',
-      'hint-interpolation.js'
+      'hint-interpolation.js',
+      'test/*_test.js'
     ],
 
 
