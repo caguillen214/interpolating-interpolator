bd3e9aca0104968d8b1ceb060e146e190d280b37
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-04 12:49:03 -0700

    style(): remove unnecessary variables

--- hint-interpolation.js
+++ hint-interpolation.js
@@ -5,19 +5,19 @@ var buildMessage = require('./lib/buildMessage');
 
 angular.module('ngHintInterpolation', [])
   .config(['$provide', function($provide) {
-    $provide.decorator('$interpolate', ['$delegate', '$timeout', function($delegate, $timeout) {
+    $provide.decorator('$interpolate', ['$delegate', function($delegate) {
       var interpolateWrapper = function() {
         var interpolationFn = $delegate.apply(this, arguments);
         if(interpolationFn) {
-          var parts = getAllParts(arguments[0],$delegate.startSymbol(),$delegate.endSymbol());
-          var temp = interpolationFnWrap(interpolationFn,arguments, parts);
+          var parts = getAllParts(arguments[0], $delegate.startSymbol(), $delegate.endSymbol());
+          var temp = interpolationFnWrap(interpolationFn, arguments, parts);
           return temp;
         }
       };
       var interpolationFnWrap = function(interpolationFn, interpolationArgs, allParts) {
         return function(){
           var result = interpolationFn.apply(this, arguments);
-          buildMessage(allParts, interpolationArgs[0].trim(), arguments[0], $timeout);
+          buildMessage(allParts, interpolationArgs[0].trim(), arguments[0]);
           return result;
         };
       };
