71ba25905943d09ecda6400f1db8a57635962b3a
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-04 12:09:29 -0700

    fix(suggestIt): remove mandatory suggestion check
    A check was added in the refactor that required a suggestion to always
    be present for a message to print. However, there may be cases where
    no suggestion is given. A test case clarifying when a suggestion should be
    given is added and this check is removed.

--- hint-interpolation_test.js
+++ hint-interpolation_test.js
@@ -3,13 +3,15 @@
 var hintLog = angular.hint;
 
 describe('hintInterpolation integration test', function() {
-  var $rootScope, $compile;
+  var $rootScope, $compile, $controller;
 
   beforeEach(module('ngHintInterpolation'));
-  beforeEach(inject(function(_$rootScope_, _$compile_) {
+  beforeEach(inject(function(_$rootScope_, _$compile_, _$controller_) {
     $rootScope = _$rootScope_;
     $compile = _$compile_;
+    $controller = _$controller_;
   }));
+
 
   it('should log a message using the hintLog pipeline when interpolation is undefined', function() {
     var html = '<a ng-href="{{data.results[0].urls.main_url}}"></a>';
@@ -17,5 +19,20 @@ describe('hintInterpolation integration test', function() {
     var log = hintLog.flush();
     expect(Object.keys(log['Interpolation'])).toEqual([' "data" was found to be undefined in ' +
       '"{{data.results[0].urls.main_url}}".']);
+  });
+
+
+  it('should make suggest close matches to undefined values if they are on the scope', function() {
+    var html = '<a ng-href="{{data.results[0].urls.main_url}}"></a>';
+    var scope = $rootScope.$new();
+    var ctrl = $controller(function() {
+      scope.datas = {
+        results: ['something']
+      };
+    });
+    $compile(html)(scope);
+    var log = hintLog.flush();
+    expect(Object.keys(log['Interpolation'])).toEqual([' "data" was found to be undefined in ' +
+      '"{{data.results[0].urls.main_url}}". Try: "datas"']);
   });
 });
