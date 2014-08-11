03634166d1ebe0d03a01053253f3c9e4e8271eec
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-11 11:31:32 -0700

    fix(quotes): handle interpolated quotes
    Some complex interpolation sequences were broken by the presence of quotes.

--- test/getOperands_test.js
+++ test/getOperands_test.js
@@ -6,13 +6,30 @@ describe('getOperands()', function() {
     var result = getOperands(toSend, '{{','}}');
     var expected = ['data ',' data.name'];
     expect(result).toEqual(expected);
+
     toSend = 'data[0] + data.["test"]';
     result = getOperands(toSend, '{{','}}');
-    expected = ['data[0] ',' data.["test"]'];
+    expected = ['data[0] ',' data.[]'];
     expect(result).toEqual(expected);
+
     toSend = 'getData() + getData("test")';
     result = getOperands(toSend, '{{','}}');
-    expected = ['getData() ',' getData("test")'];
+    expected = [ 'getData   ', ' getData  ' ];
+    expect(result).toEqual(expected);
+
+    toSend = 'a && b || c && d';
+    result = getOperands(toSend, '{{','}}');
+    expected = [ 'a ', '', ' b ', '', ' c ', '', ' d' ];
+    expect(result).toEqual(expected);
+
+    toSend = '(a + b) + ((c + d))';
+    result = getOperands(toSend, '{{','}}');
+    expected = [' a ', ' b  ', '   c ', ' d  '];
+    expect(result).toEqual(expected);
+
+    toSend = '"(a + b)" + variable + "((c + d))"';
+    result = getOperands(toSend, '{{','}}');
+    expected = [ ' ', ' variable ', ' ' ];
     expect(result).toEqual(expected);
   });
 });
