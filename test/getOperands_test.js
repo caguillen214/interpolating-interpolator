4a1a9584fa22d54493aaa5b7ae5a79c3fdd3f92a
Carlo s A. Guillen <caguillen@google.com>
2014-08-13 11:57:33 -0700

    fix(getOperands): change replace value to `|` so that it will appropriately split in the next function call

--- test/getOperands_test.js
+++ test/getOperands_test.js
@@ -4,32 +4,37 @@ describe('getOperands()', function() {
   it('should return the operands of the interpolation expression', function() {
     var toSend = 'data + data.name';
     var result = getOperands(toSend, '{{','}}');
-    var expected = ['data ',' data.name'];
+    var expected = ['data','data.name'];
     expect(result).toEqual(expected);
 
     toSend = 'data[0] + data.["test"]';
     result = getOperands(toSend, '{{','}}');
-    expected = ['data[0] ',' data.[]'];
+    expected = ['data[0]','data.[]'];
     expect(result).toEqual(expected);
 
     toSend = 'getData() + getData("test")';
     result = getOperands(toSend, '{{','}}');
-    expected = [ 'getData   ', ' getData  ' ];
+    expected = [ 'getData', 'getData' ];
+    expect(result).toEqual(expected);
+
+    toSend = 'getData() + getData(data.text)';
+    result = getOperands(toSend, '{{','}}');
+    expected = [ 'getData', 'getData', 'data.text'];
     expect(result).toEqual(expected);
 
     toSend = 'a && b || c && d';
     result = getOperands(toSend, '{{','}}');
-    expected = [ 'a ', '', ' b ', '', ' c ', '', ' d' ];
+    expected = [ 'a', 'b', 'c', 'd' ];
     expect(result).toEqual(expected);
 
     toSend = '(a + b) + ((c + d))';
     result = getOperands(toSend, '{{','}}');
-    expected = [' a ', ' b  ', '   c ', ' d  '];
+    expected = ['a', 'b', 'c', 'd'];
     expect(result).toEqual(expected);
 
     toSend = '"(a + b)" + variable + "((c + d))"';
     result = getOperands(toSend, '{{','}}');
-    expected = [ ' ', ' variable ', ' ' ];
+    expected = ['variable'];
     expect(result).toEqual(expected);
   });
 });
