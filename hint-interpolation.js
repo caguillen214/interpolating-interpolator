9ba3839b239f6d6631b9f8ccd22149103643bec2
Carlo s A. Guillen <caguillen@google.com>
2014-08-14 13:22:40 -0700

    chore(*): clean up code

--- hint-interpolation.js
+++ hint-interpolation.js
@@ -10,8 +10,9 @@ angular.module('ngHintInterpolation', [])
         var interpolationFn = $delegate.apply(this, arguments);
         if(interpolationFn) {
           var temp;
-          if(arguments[0].indexOf('?') === -1 && arguments[0].indexOf(':') === -1) {
-            var parts = getAllParts(arguments[0], $delegate.startSymbol(), $delegate.endSymbol());
+          var interpolation = arguments[0];
+          if(interpolation.indexOf('?') === -1 && interpolation.indexOf(':') === -1) {
+            var parts = getAllParts(interpolation, $delegate.startSymbol(), $delegate.endSymbol());
           }
           temp = interpolationFnWrap(interpolationFn, arguments, parts || []);
           return temp;
@@ -20,11 +21,12 @@ angular.module('ngHintInterpolation', [])
       var interpolationFnWrap = function(interpolationFn, interpolationArgs, allParts) {
         return function(){
           var result = interpolationFn.apply(this, arguments);
-          buildMessage(allParts, interpolationArgs[0].trim(), arguments[0]);
+          var originalInterpolation = interpolationArgs[0].trim();
+          buildMessage(allParts, originalInterpolation, arguments[0]);
           return result;
         };
       };
-      angular.extend(interpolateWrapper,$delegate);
+      angular.extend(interpolateWrapper, $delegate);
       return interpolateWrapper;
     }]);
   }]);
